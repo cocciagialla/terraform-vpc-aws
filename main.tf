26ca3b050f099a678392f8fd6204b42a549b1887
Gianluigi 'cocciagialla' Mammarella <cocciagialla@gmail.com>
2024-10-20 04:19:26 +0200

    Update to version 5.14.0

--- main.tf
+++ main.tf
@@ -2,8 +2,9 @@ data "aws_availability_zones" "available" {}
 
 locals {
   azs      = slice(data.aws_availability_zones.available.names, 0, 2)
-  public_subnets = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k)]
-  private_subnets  = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k)]
+  private_subnets     = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k)]
+  public_subnets      = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k + 4)]
+#   public_subnets      = [for k, v in local.azs : cidrsubnet(var.vpc_cidr, 8, k)]
   cluster_name = var.cluster_name
   tags = {
     env         = var.environment
@@ -19,7 +20,7 @@ locals {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "5.2.0"
+  version = "5.14.0"
 
   name = local.cluster_name
   cidr = var.vpc_cidr
@@ -28,6 +29,7 @@ module "vpc" {
   public_subnets  = local.public_subnets
   private_subnets = var.create_private_subnets ? local.private_subnets : []
 
+  map_public_ip_on_launch = var.create_private_subnets ? false : true
   enable_nat_gateway      = var.create_private_subnets ? true : false
   single_nat_gateway      = false
   one_nat_gateway_per_az  = true
